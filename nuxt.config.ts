33b35cc42fef2329548c49f42717e9730c926a88
DCamron Dunlap <camron153@gmail.comgit config --global user.email camron153@gmail.com>
2024-05-22 20:05:17 -0400

    installed radix testing out different components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import path from 'path';
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   routeRules: {
@@ -7,65 +7,23 @@ export default defineNuxtConfig({
     '/': { prerender: true },
   },
   alias: {
-    assets: "/rootDir>/assets"
+    assets: '@/assets'
   },
   modules: [
     '@nuxtjs/tailwindcss',
-    'nuxt-primevue',
     '@nuxtjs/color-mode',
-    'nuxt-icon-tw'
+    'nuxt-icon-tw',
+    'radix-vue/nuxt'
 
   ],
-  primevue: {
-    options: {
-      unstyled: true
-    },
-    importPT: { as: 'Lara', from: '~/presets/lara' }       //import and apply preset
-  },
-  tailwindcss: {
-    config: {
-      content: [
-        "presets/**/*.{js,vue,ts}",
-      ],
-      theme: {
-        extend: {
-            colors: {
-                primary: 'rgb(var(--primary))',
-                'primary-inverse': 'rgb(var(--primary-inverse))',
-                'primary-hover': 'rgb(var(--primary-hover))',
-                'primary-active-color': 'rgb(var(--primary-active-color))',
-
-                'primary-highlight': 'rgb(var(--primary)/var(--primary-highlight-opacity))',
-                'primary-highlight-inverse': 'rgb(var(--primary-highlight-inverse))',
-                'primary-highlight-hover': 'rgb(var(--primary)/var(--primary-highlight-hover-opacity))',
-
-                'primary-50': 'rgb(var(--primary-50))',
-                'primary-100': 'rgb(var(--primary-100))',
-                'primary-200': 'rgb(var(--primary-200))',
-                'primary-300': 'rgb(var(--primary-300))',
-                'primary-400': 'rgb(var(--primary-400))',
-                'primary-500': 'rgb(var(--primary-500))',
-                'primary-600': 'rgb(var(--primary-600))',
-                'primary-700': 'rgb(var(--primary-700))',
-                'primary-800': 'rgb(var(--primary-800))',
-                'primary-900': 'rgb(var(--primary-900))',
-                'primary-950': 'rgb(var(--primary-950))',
-
-                'surface-0': 'rgb(var(--surface-0))',
-                'surface-50': 'rgb(var(--surface-50))',
-                'surface-100': 'rgb(var(--surface-100))',
-                'surface-200': 'rgb(var(--surface-200))',
-                'surface-300': 'rgb(var(--surface-300))',
-                'surface-400': 'rgb(var(--surface-400))',
-                'surface-500': 'rgb(var(--surface-500))',
-                'surface-600': 'rgb(var(--surface-600))',
-                'surface-700': 'rgb(var(--surface-700))',
-                'surface-800': 'rgb(var(--surface-800))',
-                'surface-900': 'rgb(var(--surface-900))',
-                'surface-950': 'rgb(var(--surface-950))'
-            }
-        }
-    }
-  },
+  imports: {
+    dirs: [
+      // Scan top-level modules
+      'composables',
+      // ... or scan modules nested one level deep with a specific name and file extension
+      'composables/*/index.{ts,js,mjs,mts}',
+      // ... or scan all modules within given directory
+      'composables/**'
+    ]
   }
 });
